bcf6dcb6c20836bac056469af6c75db3c1a80302
kris-na <67404646+kris-na@users.noreply.github.com>
2020-06-25 19:34:02 +0530

    Update effect.js

--- assets/effect.js
+++ assets/effect.js
@@ -177,8 +177,8 @@ $('document').ready(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(4500);
 			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
+				$("p:nth-child(49)").fadeIn('slow').promise().done(function () {
+					$('.cake').fadeOut('fast');
 				});
                          
 	 	
